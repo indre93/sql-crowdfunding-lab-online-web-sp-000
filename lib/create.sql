a0a0c598a5ffedebc9d3ded6039f751f2d0072bf
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 19:59:02 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,10 @@
 CREATE TABLE projects (
 id INTEGER PRIMARY KEY,
-name TEXT,
-age INTEGER,
-breed TEXT);
+title TEXT,
+category TEXT,
+funding_goal INTEGER,
+start_date INTEGER,
+end_date INTEGER);
 
 CREATE TABLE users (
 id INTEGER PRIMARY KEY,
