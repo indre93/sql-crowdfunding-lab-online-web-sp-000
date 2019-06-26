dc4f8dec1ac9da81ba564a97f3b1d538841ec31e
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 20:42:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,11 +8,12 @@
 
 # [["Animal shelter needs dog food", 210], ["Help me buy a guitar", 98], ["Help save birds of paradise"..."The next Harry Potter", 120], ["The next Inna-Gadda-Davida", 342], ["Voldement needs a body", 489]]
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-  "SELECT projects.title, SUM(pledges.amount)
+  "SELECT projects.title
   FROM projects
   INNER JOIN pledges
   ON projects.id = pledges.project_id
-  JOIN users ON pledges.user_id = users.id"
+  JOIN users ON pledges.user_id = users.id
+  ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
