c11ebb94542026fd74d5454142045c255fed03e0
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 20:46:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   INNER JOIN pledges
   ON projects.id = pledges.project_id
   JOIN users ON pledges.user_id = users.id
-  GROUP BY users.name
+  GROUP BY projects.title
   ORDER BY SUM(pledges.amount)"
 end
 
